5cacd4dfdc7974f138c93a996f727eeec369f811
ShaikRehamunnisaBegum <135826350+ShaikRehamunnisaBegum@users.noreply.github.com>
2023-06-08 18:20:46 +0530

    Tall.java

---
+++ Tall.java
@@ -0,0 +1,43 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+class Tall
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		Scanner sc=new Scanner (System.in);
+
+		int t=sc.nextInt();
+
+		for(int i=0;i<t;i++){
+
+		    int X=sc.nextInt();
+
+            int Y=sc.nextInt();
+
+            if(X<Y){
+
+                System.out.println("B");
+
+            }else{
+
+                System.out.println("A");
+
+            }
+
+		}
+
+	}
+
+}
