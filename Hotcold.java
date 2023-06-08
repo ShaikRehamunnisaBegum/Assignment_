64764c0e54581b946f7b8028259063401b2f911e
ShaikRehamunnisaBegum <135826350+ShaikRehamunnisaBegum@users.noreply.github.com>
2023-06-08 18:40:32 +0530

    Hotcold.java

---
+++ Hotcold.java
@@ -0,0 +1,52 @@
+
+
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+
+
+class Hotcold
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+		
+
+		Scanner sc=new Scanner(System.in);
+
+		int t=sc.nextInt();
+
+		while(t-->0)
+
+		{
+
+		    int c=sc.nextInt();
+
+		    if(c<=20)
+
+		    {
+
+		        System.out.println("COLD");
+
+		    }
+
+		    else{
+
+		        System.out.println("HOT");
+
+		    }
+
+		}
+
+	}
+
+}
