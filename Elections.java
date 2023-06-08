ac9ffaad94277fc135808d6501bb6d5fe308222a
ShaikRehamunnisaBegum <135826350+ShaikRehamunnisaBegum@users.noreply.github.com>
2023-06-08 18:53:31 +0530

    Elections.java

---
+++ Elections.java
@@ -0,0 +1,52 @@
+
+
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+
+
+class Elections
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+		
+
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		while(t--> 0){
+
+		    int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+
+		    if (a>50)
+
+		        System.out.println("A");
+
+		    else if (b > 50)
+
+		        System.out.println("B");
+
+		    else if (c > 50)
+
+		        System.out.println("C");
+
+		  else
+
+		    System.out.println("NOTA");
+
+		}
+
+	}
+
+}
