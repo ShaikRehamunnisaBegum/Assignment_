4ae007f55fb89f7571b7fa37ec6c7f90ddeaf123
ShaikRehamunnisaBegum <135826350+ShaikRehamunnisaBegum@users.noreply.github.com>
2023-06-08 19:35:54 +0530

    Age.java

---
+++ Age.java
@@ -0,0 +1,50 @@
+
+
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+
+
+class Age
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+		
+
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		for (int i=0; i<t; i++)
+
+		{
+
+		    int x = sc.nextInt();
+
+		    int y = sc.nextInt();
+
+		    int a = sc.nextInt();
+
+		    if (a >= x && a < y)
+
+		    System.out.println("yes");
+
+		    else
+
+		    System.out.println("no");
+
+		}
+
+	}
+
+}
